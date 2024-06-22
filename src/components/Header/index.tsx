99cd1169d1bc9c4abbb6b8d07736f21201814ee2
Tamilvanan <tamilvanan@doodleblue.com>
2024-06-22 23:40:36 +0530

    header hide and show changes

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import SearchInput from "../SearchInput";
-import Button from "../Button";
 import LazyImage from "../LazyImage";
 
 const Header = () => {
+  const [isVisible, setIsVisible] = useState(true);
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollPos = window.scrollY;
+      setIsVisible(currentScrollPos < 10 || prevScrollPos > currentScrollPos);
+      setPrevScrollPos(currentScrollPos);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [prevScrollPos]);
+
   return (
-    <header className="bg-purple-400 bg-opacity-20 backdrop-filter backdrop-blur-md border border-gray-100 rounded-full shadow-lg fixed top-5 left-0 right-0 mx-auto w-4/5 z-10">
+    <header
+      className={`bg-purple-400 bg-opacity-20 backdrop-filter backdrop-blur-md border border-gray-100 rounded-full shadow-lg fixed top-5 left-0 right-0 mx-auto w-4/5 z-10 transition-all duration-300 ${
+        isVisible ? "opacity-100" : "opacity-0 pointer-events-none"
+      }`}
+    >
       <div className="container mx-auto px-4 py-2">
         <nav className="flex justify-between items-center">
           <LazyImage
@@ -28,7 +47,7 @@ const Header = () => {
                 className="text-gray-600 text-sm mr-4 hover:text-gray-400"
               >
                 Login
-              </a>{" "}
+              </a>
             </div>
           </div>
         </nav>
